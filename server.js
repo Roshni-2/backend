76b734173a85401986fd38b02a1602fecd53c49b
Roshni-2 <roshnimanivannan2001@gmail.com>
2025-10-26 17:30:05 +0530

    Initial backend commit

---
+++ server.js
@@ -0,0 +1,25 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import userRoutes from "./routes/userRoutes.js";
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+mongoose.connect("mongodb+srv://roshni_db_user:Roshni%402025@mern.4xfitnp.mongodb.net/Mernapp")
+.then(console.log("MongoDB Connected"))
+.catch((error) => console.log(error))
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+// Routes
+app.use("/api/users", userRoutes);
+
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
