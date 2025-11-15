391b1ec7b0d3f29b870f4f5ec7b9774292988095
Roshni-2 <roshnimanivannan2001@gmail.com>
2025-11-16 02:33:52 +0530

    server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-  origin: ["https://webpage-dusky-delta.vercel.app/"],
+  origin: ["https://webpage-dusky-delta.vercel.app"],
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true,
 }));
